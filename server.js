1efc622d9734e2ec9f235da2490064235e39a8bf
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2024-03-30 22:26:26 +0530

    gql-project

---
+++ server.js
@@ -0,0 +1,25 @@
+// Commonjs Way
+const express = require("express");
+const server = express();
+const glHandler = require("express-graphql");
+const schema = require("./schema");
+const cors = require("cors");
+
+server.use(cors());
+
+// express routing
+// HTTP --> GET, PUT, POST , DELETE (ALL)
+
+server.all(
+  "/graphql",
+  glHandler({
+    schema,
+    graphiql: true,
+  })
+);
+
+// port 3001
+let PORT = 3001;
+server.listen(PORT, () => {
+  console.log("server is running port", PORT);
+});
